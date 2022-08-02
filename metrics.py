898a4399be018f1f4a98c78313f0b1c34cd9d91e
snknitin <snk.nitin@gmail.com>
2022-08-02 18:05:31 +0530

    Custom metric

---
+++ metrics.py
@@ -0,0 +1,50 @@
+from typing import Optional
+from torchmetrics import Metric
+import torch
+from torchmetrics import Accuracy,MeanAbsolutePercentageError,SymmetricMeanAbsolutePercentageError
+from torchmetrics import MeanAbsoluteError,MeanSquaredError,R2Score,KLDivergence,WeightedMeanAbsolutePercentageError
+
+
+class CustomMetrics(Metric):
+    # Set to True if the metric is differentiable else set to False
+    is_differentiable: Optional[bool] = None
+
+    # Set to True if the metric reaches it optimal value when the metric is maximized.
+    # Set to False if it when the metric is minimized.
+    higher_is_better: Optional[bool] = True
+
+    # Set to True if the metric during 'update' requires access to the global metric
+    # state for its calculations. If not, setting this to False indicates that all
+    # batch states are independent and we will optimize the runtime of 'forward'
+    full_state_update: bool = False
+
+    def __init__(self):
+        super().__init__()
+        self.add_state("mae", default=torch.tensor(0), dist_reduce_fx="mean")
+        self.add_state("mse", default=torch.tensor(0), dist_reduce_fx="mean")
+        self.add_state("rmse", default=torch.tensor(0), dist_reduce_fx="mean")
+        self.add_state("r2", default=torch.tensor(0), dist_reduce_fx="mean")
+
+
+    def update(self, preds: torch.Tensor, target: torch.Tensor):
+        #preds, target = self._input_format(preds, target)
+        assert preds.shape == target.shape
+        self.mae = MeanAbsoluteError()
+        self.mse = MeanSquaredError()
+        self.r2 = R2Score(multioutput='uniform_average')
+
+
+    def compute(self):
+        return {'mae':self.mae(preds, targets),
+                'mse':self.mse(preds,targets),
+                'rmse':torch.sqrt(torch.mean(self.mse(preds,targets))),
+                'r2':self.r2(preds, targets)}
+
+
+if __name__ == "__main__":
+    preds = torch.tensor([28,145,69,84,92,50,39],dtype=torch.float)
+    targets = torch.tensor([24,120,73,84,86,38,48],dtype=torch.float)
+    metric = CustomMetrics()
+    metric(preds,targets)
+    acc = metric.compute()
+    print(acc)
